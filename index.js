a303d48fcef31dadb5e61f43bd525618103e9224
UnisLTU <109076072+UnisLTU@users.noreply.github.com>
2023-11-16 22:15:44 +0200

    20231116 CSS

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ console.log(object1.func(1, 2, object1.age));
 
 // console.log(object1.otherFunc(1, 2, object2.age));
 
-//hoisting does not work on arrow functions
+//hoisting does not work on arrow functions. Also does not work on let, const
 // const otherAddNumbers = (a, b, age) => {
 //   return a + b + age;
 // };
 
-//hoisting works/ function addNumbers
+//hoisting works/function addNumbers move to top. Also works on var
 function addNumbers(a, b, age) {
   return a + b + age;
 }
